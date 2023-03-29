4c85b1eff8a17581740b890fe6be6cfa7888445c
Yang <94277424+YaaangZ@users.noreply.github.com>
2023-03-29 14:19:20 +0100

    update mainController.py

--- Application/mainController.py
+++ Application/mainController.py
@@ -1,4 +1,7 @@
+import json
 import traceback
+import pandas as pd
+
 
 from flask import Flask, render_template, jsonify
 from sqlalchemy import create_engine, text
@@ -33,15 +36,21 @@ def get_stations():
             rows = conn.execute(text(sql)).fetchall()
             print('#found {} stations', len(rows), rows)
         return jsonify([row._asdict() for row in rows])  # use this formula to turn the rows into a list of dicts
-        # return render_template("index.html", apikey=config.APIKEY)
     except:
         print(traceback.format_exc())
         return "error in get_stations", 404
 
 
 @app.route("/occupancy/<int:station_id>")
-def get_occupancy():
-    return render_template("occupancy.html", apikey=config.APIKEY)
+def get_occupancy(station_id):
+    engine = get_engine()
+    df = pd.read_sql_query("select * from availability where number = %(number)s", engine, params={"number": station_id})
+    df['last_update_date'] = pd.to_datetime(df.last_update, unit='ms')
+    df.set_index('last_update_date', inplace=True)
+    res = df['available_bike_stands'].resample('1d').mean()
+    # res['dt'] = df.index
+    print(res)
+    return jsonify(data=json.dumps(list(zip(map(lambda x: x.isoformat(), res.index), res.values))))
 
 
 @app.route("/stations/<int:station_id>")
