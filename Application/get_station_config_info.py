9021698b349d5b3eecd17db6ffb442cb40189606
Yang <94277424+YaaangZ@users.noreply.github.com>
2023-03-29 13:29:33 +0100

    add marker on the google map

---
+++ Application/get_station_config_info.py
@@ -0,0 +1,11 @@
+STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
+JCKEY = "92a580e0a48e1f31ade6e2c8c40372a25f10c56e"
+NAME = "dublin"
+
+USER = "admin"
+PASSWORD = "group8ucd"
+# Yang's RDS
+HOST = "dbbikes.cqqckwqnmywv.eu-west-1.rds.amazonaws.com"
+DATABASE = "dbbikes"
+
+PRE_PATH = "/home/ubuntu/folder_dbbikes/log/"
--- Application/mainController.py
+++ Application/mainController.py
@@ -1,12 +1,52 @@
-from flask import Flask, render_template
+import traceback
+
+from flask import Flask, render_template, jsonify
+from sqlalchemy import create_engine, text
+from get_station_config_info import *
+
 import config
 
 app = Flask(__name__, static_url_path="/")
 
 
+def get_engine():
+    """
+    get engine to handle database
+    :return: sqlalchemy engine
+    """
+    engine = create_engine('mysql://{}:{}@{}/{}'.format(USER, PASSWORD, HOST, DATABASE), echo=True)
+    return engine
+
+
 @app.route("/")
 def main():
     return render_template("index.html", apikey=config.APIKEY)
+
+
+@app.route("/stations")
+# @functools.lru_cache(maxsize=128)
+def get_stations():
+    engine = get_engine()
+    sql = "select * from station;"
+    try:
+        with engine.connect() as conn:
+            rows = conn.execute(text(sql)).fetchall()
+            print('#found {} stations', len(rows), rows)
+        return jsonify([row._asdict() for row in rows])  # use this formula to turn the rows into a list of dicts
+        # return render_template("index.html", apikey=config.APIKEY)
+    except:
+        print(traceback.format_exc())
+        return "error in get_stations", 404
+
+
+@app.route("/occupancy/<int:station_id>")
+def get_occupancy():
+    return render_template("occupancy.html", apikey=config.APIKEY)
+
+
+@app.route("/stations/<int:station_id>")
+def get_station_by_id():
+    return render_template("stations.html", apikey=config.APIKEY)
 
 
 if __name__ == '__main__':
