5bd159c54ac310590a4bbc5ac6071d324980f1d6
winnie imafidon <88711013+Winnie101995@users.noreply.github.com>
2023-04-10 09:23:08 +0100

    front-end//sidebar and buttons working,

--- dbikes_flask_app/static/index.js
+++ dbikes_flask_app/static/index.js
@@ -1,3 +1,33 @@
+var directionsService;
+
+var directionsRenderer;
+
+let lastClickedMarker = null;
+
+let currentRoute
+
+function initMap() {
+
+  directionsService = new google.maps.DirectionsService();
+  directionsRenderer = new google.maps.DirectionsRenderer();
+
+  // The location of Dublin
+  const dublinLatLng = { lat: 53.350140, lng: -6.266155};
+
+  // The map, centered at Dublin
+  map = new google.maps.Map(document.getElementById("map"),{
+    zoom: 15,
+    center: dublinLatLng,
+
+  });
+  map.setOptions({styles: styles['hide']});
+  getStations();
+  directionsRenderer.setMap(map); // add the renderer to the map
+
+  // new AutocompleteDirectionsHandler(map);
+
+}
+
 
 function addMarkers(stations){
   stations.forEach(station =>{
@@ -14,20 +44,40 @@ function addMarkers(stations){
     </p>`;
 
     let markerIcon;
-
+    document.getElementById('availablebike-button').addEventListener('click', function(){
+      console.log('hi')
     if (station.available_bikes >= 25) {
-      markerIcon = {
+      marker.setIcon({
         url: document.getElementById('my-element-1').dataset.imageUrl,
-      }
+      })
     } else if (station.available_bikes >= 10 && station.available_bikes <=24) {
-      markerIcon = {
+      marker.setIcon({
         url: document.getElementById('my-element-2').dataset.imageUrl,
-      }
+      })
     } else if (station.available_bikes <= 9) {
-      markerIcon = {
+      marker.setIcon({
         url: document.getElementById('my-element-3').dataset.imageUrl,
-      }
+      })
     } 
+  })
+
+
+  document.getElementById('availablebikestand-button').addEventListener('click', function(){
+    console.log('hi')
+  if (station.available_bike_stands >= 25) {
+    marker.setIcon({
+      url: document.getElementById('my-element-1').dataset.imageUrl,
+    })
+  } else if (station.available_bike_stands >= 10 && station.available_bikes <=24) {
+    marker.setIcon({
+      url: document.getElementById('my-element-2').dataset.imageUrl,
+    })
+  } else if (station.available_bike_stands <= 9) {
+    marker.setIcon({
+      url: document.getElementById('my-element-3').dataset.imageUrl,
+    })
+  } 
+})
     var marker = new google.maps.Marker({
         position: {
          lat: lat,
@@ -36,6 +86,9 @@ function addMarkers(stations){
         map: map,
         icon: markerIcon,
         title: station.name,
+        address: station.address,
+        position_lat: station.position_lat,
+        position_lng: station.position_lat,
         station_number: station.number,
         animation: google.maps.Animation.DROP // Add animation property
 
@@ -78,14 +131,61 @@ function addMarkers(stations){
       marker.setAnimation(null);
 
     });
+    marker.addListener('click', () => {
 
-    marker.addListener('click', createRoute())
- });
+      // If this is the first marker clicked, fill in the "from" field
+      if (!lastClickedMarker) {
+        document.getElementById('journeyfrom').value = marker.address;
+        lastClickedMarker = marker;
+      } else {
+        // Otherwise, fill in the "to" field and create a route
+    
+        const from = lastClickedMarker.getPosition();
+        const to = marker.getPosition();
+        const request = {
+          origin: from,
+          destination: to,
+          travelMode: 'WALKING',
+        };
+
+            // Clear any existing route
+    if (currentRoute) {
+      currentRoute.setMap(null);
+    }
+   
+        directionsService.route(request, function(result, status) {
+            if (status === 'OK') {
+              // Route was successfully calculated
+              directionsRenderer.setDirections(result);
+              openNav()
+
+            } else if (status === 'ZERO_RESULTS') {
+              // No route could be found between the origin and destination
+              console.log('No route found.');
+            } else if (status === 'INVALID_REQUEST') {
+              // Request was missing required parameters
+              console.log('Invalid request.');
+            } else if (status === 'OVER_QUERY_LIMIT') {
+              // Request exceeded the usage limits for the API
+              console.log('API usage limit exceeded.');
+            } else if (status === 'REQUEST_DENIED') {
+              // Request was denied, possibly due to missing or invalid API key
+              console.log('Request denied.');
+            } else if (status === 'UNKNOWN_ERROR') {
+              // An unknown error occurred
+              console.log('Unknown error.');
+            }
+          });
+                
+        document.getElementById('journeyto').value = marker.address;
+        lastClickedMarker = null;
+      }
+    });
+  });
 }
 
-function createRoute(){
-  
-}
+
+
 function getStations(){
 
     fetch("/stations")
@@ -100,35 +200,6 @@ function getStations(){
 }
 
 
-// function clickEvent(id){
-
-// }
-
-// function showStaticData(id){
-
-
-// }
-
-
-
-function initMap() {
-    // The location of Dublin
-    const dublinLatLng = { lat: 53.350140, lng: -6.266155};
-
-
-    // The map, centered at Dublin
-    map = new google.maps.Map(document.getElementById("map"),{
-      zoom: 15,
-      center: dublinLatLng,
-
-    });
-    map.setOptions({styles: styles['hide']});
- 
-
-    getStations();
-    new AutocompleteDirectionsHandler(map);
-
-  }
 
      
   const styles = {
@@ -146,128 +217,126 @@ function initMap() {
   ]
   }
 // google api functionality 
-  class AutocompleteDirectionsHandler {
-    map;
-    originPlaceId;
-    destinationPlaceId;
-    travelMode;
-    directionsService;
-    directionsRenderer;
-    constructor(map) {
-      this.map = map;
-      this.originPlaceId = "";
-      this.destinationPlaceId = "";
-      this.travelMode = google.maps.TravelMode.WALKING;
-      this.directionsService = new google.maps.DirectionsService();
-      this.directionsRenderer = new google.maps.DirectionsRenderer({
-        polylineOptions: {
-          strokeColor: "black",
-        },
-      });
-      this.directionsRenderer.setMap(map);
+  // class AutocompleteDirectionsHandler {
+  //   map;
+  //   originPlaceId;
+  //   destinationPlaceId;
+  //   travelMode;
+  //   directionsService;
+  //   directionsRenderer;
+  //   constructor(map) {
+  //     this.map = map;
+  //     this.originPlaceId = "";
+  //     this.destinationPlaceId = "";
+  //     this.travelMode = google.maps.TravelMode.WALKING;
+  //     this.directionsService = new google.maps.DirectionsService();
+  //     this.directionsRenderer = new google.maps.DirectionsRenderer({
+  //       polylineOptions: {
+  //         strokeColor: "black",
+  //       },
+  //     });
+  //     this.directionsRenderer.setMap(map);
 
 
-    const originInput = document.getElementById("journeyfrom");
-    const destinationInput = document.getElementById("journeyto");
-    const modeSelector = document.getElementById("mode-selector");
-    // Specify just the place data fields that you need.
-    const originAutocomplete = new google.maps.places.Autocomplete(
-      originInput,
-      { fields: ["place_id"] }
-    );
-    // Specify just the place data fields that you need.
-    const destinationAutocomplete = new google.maps.places.Autocomplete(
-      destinationInput,
-      { fields: ["place_id"] }
+  //   const originInput = document.getElementById("journeyfrom");
+  //   const destinationInput = document.getElementById("journeyto");
+  //   const modeSelector = document.getElementById("mode-selector");
+  //   // Specify just the place data fields that you need.
+  //   const originAutocomplete = new google.maps.places.Autocomplete(
+  //     originInput,
+  //     { fields: ["place_id"] }
+  //   );
+  //   // Specify just the place data fields that you need.
+  //   const destinationAutocomplete = new google.maps.places.Autocomplete(
+  //     destinationInput,
+  //     { fields: ["place_id"] }
 
-    );
-
-
-    this.setupClickListener(
-      "changemode-walking",
-      google.maps.TravelMode.WALKING
-    );
-    this.setupClickListener(
-      "changemode-transit",
-      google.maps.TravelMode.TRANSIT
-    );
-    this.setupClickListener(
-      "changemode-driving",
-      google.maps.TravelMode.DRIVING
-    );
+  //   );
 
 
-    this.setupPlaceChangedListener(originAutocomplete, "ORIG");
-    this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
-  }
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
-  setupClickListener(id, mode) {
-    const radioButton = document.getElementById(id);
+  //   this.setupClickListener(
+  //     "changemode-walking",
+  //     google.maps.TravelMode.WALKING
+  //   );
+  //   this.setupClickListener(
+  //     "changemode-transit",
+  //     google.maps.TravelMode.TRANSIT
+  //   );
+  //   this.setupClickListener(
+  //     "changemode-driving",
+  //     google.maps.TravelMode.DRIVING
+  //   );
 
-    radioButton.addEventListener("click", () => {
-      this.travelMode = mode;
-      this.route();
-    });
-  }
-  setupPlaceChangedListener(autocomplete, mode) {
-    autocomplete.bindTo("bounds", this.map);
-    autocomplete.addListener("place_changed", () => {
-      const place = autocomplete.getPlace();
 
-      if (!place.place_id) {
-        window.alert("Please select an option from the dropdown list.");
-        return;
-      }
+  //   this.setupPlaceChangedListener(originAutocomplete, "ORIG");
+  //   this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
+  // }
+  // // Sets a listener on a radio button to change the filter type on Places
+  // // Autocomplete.
+  // setupClickListener(id, mode) {
+  //   const radioButton = document.getElementById(id);
 
-      if (mode === "ORIG") {
-        this.originPlaceId = place.place_id;
-      } else {
-        this.destinationPlaceId = place.place_id;
-      }
+  //   radioButton.addEventListener("click", () => {
+  //     this.travelMode = mode;
+  //     this.route();
+  //   });
+  // }
+  // setupPlaceChangedListener(autocomplete, mode) {
+  //   autocomplete.bindTo("bounds", this.map);
+  //   autocomplete.addListener("place_changed", () => {
+  //     const place = autocomplete.getPlace();
 
-      this.route();
-    });
-  }
-  route() {
-    if (!this.originPlaceId || !this.destinationPlaceId) {
-      return;
-    }
+  //     if (!place.place_id) {
+  //       window.alert("Please select an option from the dropdown list.");
+  //       return;
+  //     }
 
-    const me = this;
+  //     if (mode === "ORIG") {
+  //       this.originPlaceId = place.place_id;
+  //     } else {
+  //       this.destinationPlaceId = place.place_id;
+  //     }
 
-    this.directionsService.route(
-      {
-        origin: { placeId: this.originPlaceId },
-        destination: { placeId: this.destinationPlaceId },
-        travelMode: this.travelMode,
-      },
-      (response, status) => {
-        if (status === "OK") {
-          me.directionsRenderer.setDirections(response);
-        } else {
-          window.alert("Directions request failed due to " + status);
-        }
-      }
+  //     this.route();
+  //   });
+  // }
+  // route() {
+  //   if (!this.originPlaceId || !this.destinationPlaceId) {
+  //     return;
+  //   }
+
+  //   const me = this;
+
+  //   this.directionsService.route(
+  //     {
+  //       origin: { placeId: this.originPlaceId },
+  //       destination: { placeId: this.destinationPlaceId },
+  //       travelMode: this.travelMode,
+  //     },
+  //     (response, status) => {
+  //       if (status === "OK") {
+  //         me.directionsRenderer.setDirections(response);
+  //       } else {
+  //         window.alert("Directions request failed due to " + status);
+  //       }
+  //     }
 
   
-    );
-    }
+  //   );
+  //   }
 
-    // event listener to click for and create a route for when marker is clicked 
 
+
+
+function availablebike(){
 
 }
-// generating a filter that calculates the closest marker to the origin and the destination
-
-
-
-
 
 
   
 
   function openNav() {
+ 
     document.getElementById("main").style.marginLeft = "25%";
     document.getElementById("mySidebar").style.width = "25%";
     document.getElementById("mySidebar").style.display = "block";
