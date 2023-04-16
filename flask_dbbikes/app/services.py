2bf3a1147ac707d31e3ff7efcbc2020ad8fdd76b
Crane <95408805+CraneWvs@users.noreply.github.com>
2023-04-16 23:42:20 +0100

    modify model path

--- flask_dbbikes/app/services.py
+++ flask_dbbikes/app/services.py
@@ -130,7 +130,8 @@ class DatbaseService:
 class ModelService:
     def __init__(self):
         # with open('data/random_forest_model.pkl', 'rb') as file:
-        with open('data/random_forest_model_new.pkl', 'rb') as file:
+        # with open('data/random_forest_model_new.pkl', 'rb') as file:
+        with open('/home/ubuntu/codes/model/random_forest_model_new.pkl', 'rb') as file:
         # with open('/Users/winnieimafidon/Documents/software-engineering/bike_rental_project/duplicateYun/COM30830-SE/flask_dbbikes/app/data/random_forest_model.pkl', 'rb') as file:
             data_model = pickle.load(file)
         self.model = data_model
